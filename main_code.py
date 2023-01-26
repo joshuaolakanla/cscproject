0bb6526db1118b8d80266c56d87b12cf876857f6
Joshua Olakanla <34883704+joshuaolakanla@users.noreply.github.com>
2023-01-27 00:08:11 +0100

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -1,48 +1,53 @@
+#Importing all the necessary libraries and modules
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.patches import Circle
 
-eph_names = ['earth', 'mars', 'jupiter', 'saturn', 'uranus', 'voyager2']
+#assigning a list of the spatial objects to a variable name
 
-def get_ephemeris(filename):
-    """Get the ephemeris data from filename."""
+space_bodies = ['earth', 'mars', 'jupiter', 'saturn', 'uranus', 'voyager2']
 
-    def parse_line(line):
-        """Parse a single line of the data file, returning position, (x,y,z)."""
-        sx, sy, sz = line[4:26], line[30:52], line[56:78]
-        return float(sx), float(sy), float(sz)
+#defining a function that can read the data from the given file through the years
+
+def get_trajectory(filename):
+    """Get the trajectory data from given file."""
+
+    def get_line(line):
+        """Get a single line of the data file, returning position (x,y,z)."""
+        x, y, z = line[4:26], line[30:52], line[56:78]
+        return float(x), float(y), float(z)
 
     dates, xyz = [], []
-    print(filename)
-    with open(filename) as fi:
-        while not fi.readline().strip() == '$$SOE':
+    with open(filename) as data_file:
+        while not data_file.readline().strip() == '$$SOE':
             pass
         while True:
-            line = fi.readline().rstrip()
+            line = data_file.readline().rstrip()
             if line == '$$EOE':
                 break
             if 'A.D.' in line:
                 date = line[25:36]
-                if date.startswith('1988-Jul'):
-                    # Stop reading data at this date.
+                if date.startswith('1988-Jul'): 
+		
+	#Take a pause from reading the data
                     break
                 dates.append(date)
             elif line.startswith(' X ='):
-                xyz.append(parse_line(line))
-            # Otherwise, this line doesn't interest us: move on.
+                xyz.append(get_line(line))
 
     return dates, np.array(xyz).T
+	
+	
+	#Defining a class that represents the trajectory of a celestial body.
 
-
-class Ephemeris:
-    """A simple class representing the trajectory of an astronomical object."""
+class Trajectory:
+    """This class represents the trajectory of a celestial object"""
 
     def __init__(self, r, c, trajectory):
         """
-        Initilize the Ephemeris object with a radius, r of the circle to plot,
+        Initilize the Trajectory object with a radius r of the circle to plot,
         colour, c, and trajectory parsed from its HORIZON file, traj.
-
         """
 
         self.r = r
@@ -53,22 +58,24 @@ class Ephemeris:
         """
         Get the x, y position of the object. If i is None, return the entire
         trajectory in an array, otherwise return the position at index i.
-
         """
 
         if i is None:
             return self.traj[:2]
-        return self.traj[0][i], self.traj[1][i]
+        try:
+            return self.traj[0][i], self.traj[1][i]
+        except IndexError:
+            pass
 
 
-# Initialize the Ephemeris objects for each of the objects.
+# Initialize the Trajectory objects for each of the objects.
 ephemerides = {
-        'earth': Ephemeris(0.3, 'tab:blue', get_ephemeris("C:/Users/Jossy/Documents/School/CSC/CSC Project/earth.txt")),
-        'mars': Ephemeris(0.3, 'tab:red', get_ephemeris("C:/Users/Jossy/Documents/School/CSC/CSC Project/mars.txt")),
-        'jupiter': Ephemeris(0.6, 'tab:orange', get_ephemeris("C:/Users/Jossy/Documents/School/CSC/CSC Project/jupiter.txt")),
-        'saturn': Ephemeris(0.5, 'brown', get_ephemeris("C:/Users/Jossy/Documents/School/CSC/CSC Project/saturn.txt")),
-        'uranus': Ephemeris(0.4, 'tab:green', get_ephemeris("C:/Users/Jossy/Documents/School/CSC/CSC Project/uranus.txt")),
-        'voyager2': Ephemeris(0.2, 'w', get_ephemeris("C:/Users/Jossy/Documents/School/CSC/CSC Project/voyager2.txt")),
+        'earth': Trajectory(0.3, 'tab:red', get_trajectory('C:/Users/user/Desktop/CSC project/earth.txt')),
+        'mars': Trajectory(0.3, 'tab:blue', get_trajectory('C:/Users/user/Desktop/CSC project/mars.txt')),
+        'jupiter': Trajectory(0.6, 'w', get_trajectory('C:/Users/user/Desktop/CSC project/jupiter.txt')),
+        'saturn': Trajectory(0.5, 'brown', get_trajectory('C:/Users/user/Desktop/CSC project/saturn.txt')),
+        'uranus': Trajectory(0.4, 'tab:green', get_trajectory('C:/Users/user/Desktop/CSC project/uranus.txt')),
+        'voyager2': Trajectory(0.2, 'tab:orange', get_trajectory('C:/Users/user/Desktop/CSC project/voyager2.txt')),
               }
 
 # Black figure with Axes panel removed.
@@ -76,11 +83,11 @@ fig, ax = plt.subplots()
 fig.patch.set_facecolor('k')
 ax.axis('off')
 
-# Initialize the objects and pack the Matplotlib artists into a list, e_objs.
+# Initialize the objects and put the Matplotlib artists into a list, e_objs.
 e_objs = []
 for eph_name, ephemeris in ephemerides.items():
     if eph_name != 'voyager2':
-        # Plot trajectory paths for objects other than Voyager 2.
+        # Plot trajectory paths for other celestial objects.
         ax.plot(*ephemeris.get_xy(), c=ephemeris.c, lw=0.5)
     # Circles represent each of the objects; start in initial positions.
     e_circ = Circle(xy=ephemeris.get_xy(0), radius=ephemeris.r,
@@ -88,20 +95,21 @@ for eph_name, ephemeris in ephemerides.items():
     ax.add_patch(e_circ)
     e_objs.append(e_circ)
 
-# This is the trajectory path for Voyager 2 (initialize as empty.)
+# Trajectory path for Voyager 2 initialized empty.
 e_line, = ax.plot([], [], c=ephemerides['voyager2'].c, lw=0.5)
 e_objs.append(e_line)
 label = ax.text(15, 15, '', c='w')
 e_objs.append(label)
 
-def init():
+def initialise():
+    "Initialising function to draw a clear frame"
     for i, ephemeris in enumerate(ephemerides.values()):
         e_objs[i].center = ephemeris.get_xy(0)
     e_line.set_data([], [])
     return e_objs
 
 def reformat_date(date):
-    """Reverse date string from YYYY-Mon-DD to DD-Mon-YYYY."""
+    """Change date string format from YYYY-Mon-DD to DD-Mon-YYYY."""
     return '-'.join(date.split('-')[::-1])
 
 def animate(j):
@@ -112,13 +120,15 @@ def animate(j):
         e_line.set_data(ephemeris.traj[0][:j], ephemeris.traj[1][:j])
     e_line.set_data(ephemerides['voyager2'].traj[0][:j],
                     ephemerides['voyager2'].traj[1][:j])
-    label.set_text(reformat_date(ephemerides['voyager2'].date_series[j]))
-
+    try:
+        label.set_text(reformat_date(ephemerides['voyager2'].date_series[j]))
+    except IndexError:
+        pass
     return e_objs
 
-lim = 25
-ax.set_xlim(-lim, lim)
-ax.set_ylim(-lim, lim)
-ani = animation.FuncAnimation(fig, animate, interval=1, init_func=init,
+limit = 25
+ax.set_xlim(-limit, limit)
+ax.set_ylim(-limit, limit)
+reference_holder = animation.FuncAnimation(fig, animate, interval=1, init_func=initialise,
                         blit=True)
 plt.show()
